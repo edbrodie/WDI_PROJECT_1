9dc1a31f1e9f427f8e13c7ebc10ad914bbd6d564
edbrodie <edward.g.brode@gmail.com>
2017-10-06 14:09:04 +0100

    Starting to write out my pseudocode

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
 console.log('working');
+
+/*
+//Pseudocode: All pseudocode will be written in this JS file prior to MVP developemnt.
+
+
+    '->' signifys pseudocode for HTML
+
+    '**' signifys pseudocode for CSS
+
+    '~' signifys pseudocode for JAVASCRIPT & JQUERY
+
+
+//pseudocode as follows (work in progress..)
+
+
+
+
+-> Setup link in the HTML for Jquery library
+
+->Create the HTML structure to allow CSS & DOM manipulation for a grid layout (4x4)
+  -> a class assigned for row1, row2, row3, ro4
+  -> a class assigned for col1, col2, col3, col4
+  -> an ID for each indiviual grid item (12 total)
+  -> input ID names into each grid element for visual identification on page load
+
+** Apply generic colour scheme to background, grid and grid items
+** Establish alignment, padding and margin to have the page present symetrically and ready for initial js/jquery/dom manipulation.
+** CSS to be left at this point until MVP is established.
+
+~ Setup and test listener to check for DOM load
+
+~ setup global variables and tie them to their HTML counterparts
+
+~ setup code to have all 12 grid items to 'show' on page load as default
+
+~ wait 10 seconds before hiding each grid item
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
